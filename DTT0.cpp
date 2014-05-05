ab393702650e9c42fb7c237a5caaee1975e9573d
Robin Sheng Yang <robiny@ece2524.ece.vt.edu>
2014-05-05 09:39:58 -0400

    Added basic flow of program

---
+++ DTT0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib>
+
+
+/* random number generator selects one as 1
+ * supress user io on screen and avoide enter usage
+ * use clock
+ * array stores single 1 and rest 0
+ * keys 1 - 4 for each row
+ * keep a score
+ * end game
+ */
+
+
+
+
+

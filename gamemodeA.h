10bd0c1a3ae52bfcd76aae15902c26b7319587f4
Khai N <khaingo@vt.edu>
2014-05-07 16:51:51 -0400

    Add the first gamemode

---
+++ gamemodeA.h
@@ -0,0 +1,61 @@
+#include <err.h>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iomanip>
+#include "board.h"
+
+class gamemodeA
+{
+private:
+	int score;
+public:
+	gamemodeA();
+	void runGame(int & state,board b,int userinput);
+};
+
+gamemodeA::gamemodeA()
+{
+//blank initilization
+score=0;
+}
+
+void gamemodeA::runGame(int & state, board b,int userinput)
+{
+	
+	if(state== 1)//playgame
+	{
+		if (b.istheoneat(userinput))
+		{
+			score++;
+			state=2;
+		}
+		else
+		{
+			state=3;
+		}
+	}		
+	else if(state== 2)//updateboard
+	{
+		b.updateBoard();	
+		state=1;
+	}
+	else if(state==3)//End game
+	{
+		std::cout<<"YOUR SCORE IS: "<<score<<std::endl;	
+		exit(EXIT_SUCCESS); 
+	}
+	else 
+	{
+		std::cout<<"SOMETHING IS WRONG"<<std::endl;
+                exit(EXIT_FAILURE);
+	
+	}
+}
+
+
+
+
+
